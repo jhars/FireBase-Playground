068b95505dae478e16a93642b0fe2505f9b98d4e
jhars <jjharlan06@gmail.com>
2015-12-20 10:02:28 -0800

    code cleaning

--- TwitterMach/MainViewController.swift
+++ TwitterMach/MainViewController.swift
@@ -10,63 +10,32 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
-
     var posts: [String: String] = [String: String]()
     var ref = Firebase(url: "https://twittermach.firebaseio.com/")
     
     @IBOutlet weak var msgInput: UITextField!
     
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
-
     }
-    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
-//        cell.textLabel!.text = self.posts[indexPath.row]
+
         var keys: Array = Array(self.posts.keys)
         cell.textLabel!.text = posts[keys[indexPath.row]] as String!
-        //myArray[0]
-        //myDictionary["firstObject"]
+// ======= myArray[0] ============ // =========== myDictionary["firstObject"] ============ //
         return cell
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        
-//        var postRef = self.ref.childByAppendingPath("Posts")
-//        var postMsg = composeTextField.text
-//        var newMsgValue = postMsg as String!
-//        
-//        var newPostRef = postRef.childByAutoId()
-//        
-//        
-//        print(newMsgValue)
-//        print("newMsgValue01")
-//        newPostRef.setValue(newMsgValue)
-//        print(newMsgValue)
-//        print("newMsgValue02")
-        
-        
-        
-        ref.observeEventType(.Value, withBlock: { snapshot in
-            
+
+            ref.observeEventType(.Value, withBlock: { snapshot in
             // * SAVE THIS * //
             print(snapshot.value.objectForKey("Posts"))
             print("did the view load?")
             
             var selfPosts = snapshot.value.objectForKey("Posts") as! [String: String]
-
-
             self.posts = selfPosts
-            
-//            print(self.posts)
-            
-//            print(Array(self.posts.values))
-            
-            
-            
             self.tableView.reloadData()
         })
     }
@@ -74,23 +43,16 @@ class MainViewController: UITableViewController {
     @IBAction func addMessage(sender: AnyObject) {
         var msg = msgInput.text
         
-                var postRef = self.ref.childByAppendingPath("Posts")
-                var newMsgValue = msg as String!
+        var postRef = self.ref.childByAppendingPath("Posts")
+        var newMsgValue = msg as String!
+        var newPostRef = postRef.childByAutoId()
         
-                var newPostRef = postRef.childByAutoId()
         newPostRef.setValue(newMsgValue)
-
-        
     }
-    
-    
-    
     // LOGOUT
     @IBAction func logout(sender: AnyObject) {
 //from DOCS on FireBase
         ref.unauth()
         self.performSegueWithIdentifier("logoutSegue", sender: self)
-    
     }
-
 }
